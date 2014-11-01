e0f843c4e786337d74aa557267d7e9c59be3fed7
Ryan Sickles <rsickles@andrew.cmu.edu>
2014-11-01 13:46:15 -0400

    Added XCode Project

---
+++ TourGuides/TourGuides/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TourGuides
+//
+//  Created by Ryan Sickles on 11/1/14.
+//  Copyright (c) 2014 sickles.ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TourGuides/TourGuides/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TourGuides
+//
+//  Created by Ryan Sickles on 11/1/14.
+//  Copyright (c) 2014 sickles.ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
